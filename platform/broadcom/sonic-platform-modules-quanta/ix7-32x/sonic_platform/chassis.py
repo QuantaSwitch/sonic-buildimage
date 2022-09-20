ee6e0871b8396a98f6183cfebe7d26741328b225
roberthong-qct <10606901@qcttw.com>
2022-09-20 16:01:56 +0800

    [platform] Add new APIs for chassis.py and fix get_system_eeprom_info for test_chassis

--- platform/broadcom/sonic-platform-modules-quanta/ix7-32x/sonic_platform/chassis.py
+++ platform/broadcom/sonic-platform-modules-quanta/ix7-32x/sonic_platform/chassis.py
@@ -149,6 +149,23 @@ class Chassis(ChassisBase):
         """
         return True
 
+    def get_position_in_parent(self):
+        """
+        Retrieves 1-based relative physical position in parent device.
+        Returns:
+            integer: The 1-based relative physical position in parent
+            device or -1 if cannot determine the position
+        """
+        return -1
+
+    def is_replaceable(self):
+        """
+        Indicate whether Chassis is replaceable.
+        Returns:
+            bool: True if it is replaceable.
+        """
+        return False
+
 ##############################################
 # Chassis methods
 ##############################################
@@ -162,6 +179,14 @@ class Chassis(ChassisBase):
             'XX:XX:XX:XX:XX:XX'
         """
         return self._eeprom.base_mac_addr()
+
+    def get_revision(self):
+        """
+        Retrieves the hardware revision of the device
+        Returns:
+            string: Revision value of device
+        """
+        return self._eeprom.revision_str()
 
     def get_serial_number(self):
         """
